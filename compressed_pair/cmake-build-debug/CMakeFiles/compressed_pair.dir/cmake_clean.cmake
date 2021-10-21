bb845523222ffecb0f02828c9c8fd56ceaf31b9c
yuliazhelt <77640129+yuliazhelt@users.noreply.github.com>
2021-10-21 20:09:57 +0300

    Add files via upload

---
+++ compressed_pair/cmake-build-debug/CMakeFiles/compressed_pair.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compressed_pair.dir/main.cpp.o"
+  "compressed_pair"
+  "compressed_pair.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/compressed_pair.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ compressed_pair/cmake-build-debug/CMakeFiles/compressed_pair.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for compressed_pair.
+# This may be replaced when dependencies are built.
